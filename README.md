6b2ba0f94c8753eda95cf43c1a17f5d7f3c7f383
habakami <153873868+habakami@users.noreply.github.com>
2024-07-18 18:09:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dev-app-class
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/habakami/dev-app-class)
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<div class = "bg-gradient-to-br from-violet-300 h-svh w-svw">
+  <div class = "h-svh max-x-xl flex flex-col gap-4 justify-center">
+    <div class = "bg-orange-200">レベル表示</div>
+    <div class = "bg-blue-200">スコア表示</div>
+    <div class = "bg-green-200 aspect-square">盤面</div>
+    <div class = "bg-yellow-200">残り時間</div>
+    <div class = "bg-pink-200">ヒントの表示スペース</div>
+  </div>
+</div>
